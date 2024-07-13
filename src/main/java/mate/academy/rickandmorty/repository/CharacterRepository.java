d6101b5eca75c206c5d822c61310f73432ea830e
Serhii Shyian <serhii.shn@gmail.com>
2024-07-13 19:34:56 +0300

    1) Remove @Repository annotation from CharacterRepository class

--- src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
+++ src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
@@ -5,9 +5,7 @@ import mate.academy.rickandmorty.model.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CharacterRepository extends JpaRepository<Character, Long>,
         JpaSpecificationExecutor<Character> {
     List<Character> findByNameContaining(String name);
